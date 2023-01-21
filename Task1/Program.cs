c613cb9e18e22648aeba4380b4b2d25c393a6c82
Bogdan <hurasy71@gmail.com>
2023-01-21 12:51:16 +1000

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число");
+int number1 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите число");
+int number2 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("number1 ^ number2 =" + Math.Pow(number1, number2));
